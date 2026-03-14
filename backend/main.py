7ad5ea661e4f4adf741560cd33f2aeae69f2a8ff
Radhika Dhama <dhamaradhika32@gmail.com>
2026-03-14 17:06:44 +0530

    Updated main.py with scheduler and CORS support

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,9 @@
 from fastapi import FastAPI
 from backend.api.routes import router
 from backend.services.scheduler import start_scheduler
+from fastapi.middleware.cors import CORSMiddleware
+
+
 
 app = FastAPI(
     title="Real-Time Satellite Tracker API"
@@ -8,6 +11,12 @@ app = FastAPI(
 
 app.include_router(router)
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.on_event("startup")
 def startup():
