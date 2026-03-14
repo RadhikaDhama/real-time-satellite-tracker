7b52856e8bed74db28820795b2812070a650cde0
Radhika Dhama <dhamaradhika32@gmail.com>
2026-03-14 17:21:17 +0530

    Enabled CORS middleware for frontend integration

--- backend/main.py
+++ backend/main.py
@@ -1,30 +1,32 @@
 from fastapi import FastAPI
-from backend.api.routes import router
-from backend.services.scheduler import start_scheduler
 from fastapi.middleware.cors import CORSMiddleware
 
+from backend.api.routes import router
+from backend.services.scheduler import start_scheduler
 
 
 app = FastAPI(
     title="Real-Time Satellite Tracker API"
 )
 
-app.include_router(router)
-
+# Enable CORS for frontend
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["*"],   # allow all origins
+    allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
 
+# Register API routes
+app.include_router(router)
+
+
 @app.on_event("startup")
 def startup():
-
     start_scheduler()
 
 
 @app.get("/")
 def home():
-
-    return {"message":"Satellite Tracker API running"}
+    return {"message": "Satellite Tracker API running"}
